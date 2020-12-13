111a27f7f7b3bdc30ce092f3f6f975ca1e378df1
jonathanmalay <jonathan.malai123@gmail.com>
2020-12-13 17:13:10 +0200

    Imported files from my project

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const connectDB = require('./config/db');
+const app = express();
+//connect database
+// connectDB();
+
+//the port number bthat the server run on
+const PORT = process.env.PORT || 5001;
+
+//the website styatic files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Init Middleware
+app.use(express.json({ extended: false })); //allow as get the data from every request body
+
+app.get('/', (req, res) => res.send('[+]API Running'));
+
+//Define Routes
+
+//the make contact route
+app.use(
+	'/api/makecontact',
+	require('./routes/api/makecontact') /*the module route for export */
+);
+
+app.listen(PORT, () => console.log(`[+]Server started on port ${PORT}`));
