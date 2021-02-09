6b97f86436878148fd721a6daaa5a4445d3e8ca9
jonathanmalay <jonathan.malai123@gmail.com>
2021-02-09 15:00:50 +0200

    Updated the terms PDF.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const connectDB = require('./config/db');
+const express = require("express");
+const path = require("path");
+const connectDB = require("./config/db");
 const app = express();
 //connect database
 // connectDB();
@@ -9,26 +9,21 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 //the website styatic files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Init Middleware
 app.use(express.json({ extended: false })); //allow as get the data from every request body
 
-app.get('/', (req, res) => res.send('[+]API Running'));
+app.get("/", (req, res) => res.send("[+]API Running"));
 
 //Define Routes
 
-
 // For send the terms file.
-app.get('/gy-t-and-c.pdf',  (req, res) => {
-    res.sendFile(__dirname + '\\static\\gy-t-and-c.pdf');
-
-})
+app.get("/gy-t-and-c.pdf", (req, res) => {
+  res.sendFile(__dirname + "/static/gy-t-and-c.pdf");
+});
 
 //the make contact route
-app.use(
-	'/api/makecontact',
-	require('./routes/api/makecontact') /*the module route for export */
-);
+app.use("/api/makecontact", require("./routes/api/makecontact") /*the module route for export */);
 
-app.listen(PORT, () => console.log(`[+]Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`[+] Server started on port ${PORT}`));
